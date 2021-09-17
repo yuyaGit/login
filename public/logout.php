5d8a06e98f33013a617dc443c3d55ea5e6d71e02
yuyagit <yuyagit@gmail.com>
2021-09-17 23:36:02 +0900

    やっぱ戻す

--- public/logout.php
+++ public/logout.php
@@ -10,7 +10,6 @@ if (!$logout = filter_input(INPUT_POST, "logout")) {
 
 //ログインしているか判定し、セッションが切れていたらログインしてくださいとメッセージを出す
 $result = UserLogic::checkLogin();
-
 if (!$result) {
     exit("セッション切れです");
 }
